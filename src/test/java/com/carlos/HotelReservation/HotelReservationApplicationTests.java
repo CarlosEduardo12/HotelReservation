abcd09a82dd6620b04c68f337e8d9e4307893955
Carlos Eduardo <carlos@MacBook-Pro-de-Carlos.local>
2021-05-01 14:24:34 -0300

    initial commit

---
+++ src/test/java/com/carlos/HotelReservation/HotelReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.HotelReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
