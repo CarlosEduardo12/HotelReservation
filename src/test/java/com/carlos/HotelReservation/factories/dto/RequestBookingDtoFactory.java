3b951dd98b9b64c1bba77c3429d4d414608df5bd
carloseduardo12 <carlosdudujunior@hotmail.com>
2021-05-06 11:10:52 -0300

    modify to different emails

--- src/test/java/com/carlos/HotelReservation/factories/dto/RequestBookingDtoFactory.java
+++ src/test/java/com/carlos/HotelReservation/factories/dto/RequestBookingDtoFactory.java
@@ -10,31 +10,31 @@ import java.time.LocalDate;
 public class RequestBookingDtoFactory {
 
     public static RequestBookingDto requestBookingDayOneCheckInOne(GuestRepository repository){
-        var guest = new Guest("ca@a.com","123");
+        var guest = new Guest("test1@alten.com","123");
         repository.save(guest);
         return new RequestBookingDto(guest.getId(), LocalDate.now().plusDays(1), 1);
     }
 
     public static RequestBookingDto requestBookingDayTwoCheckInThree(GuestRepository repository){
-        var guest = new Guest("ca@a.com","123");
+        var guest = new Guest("test2@alten.com","123");
         repository.save(guest);
         return new RequestBookingDto(guest.getId(), LocalDate.now().plusDays(2), 3);
     }
 
     public static RequestBookingDto requestBookingDayThreeCheckInOne(GuestRepository repository){
-        var guest = new Guest("ca@a.com","123");
+        var guest = new Guest("test3@alten.com","123");
         repository.save(guest);
         return new RequestBookingDto(guest.getId(), LocalDate.now().plusDays(3), 1);
     }
 
     public static RequestBookingDto requestBookingDayTenCheckInOne(GuestRepository repository){
-        var guest = new Guest("ca@a.com","123");
+        var guest = new Guest("test4@alten.com","123");
         repository.save(guest);
         return new RequestBookingDto(guest.getId(), LocalDate.now().plusDays(10), 1);
     }
 
     public static RequestBookingDto requestBookingDayThirtyCheckInOne(GuestRepository repository){
-        var guest = new Guest("ca@a.com","123");
+        var guest = new Guest("test5@alten.com","123");
         repository.save(guest);
         return new RequestBookingDto(guest.getId(), LocalDate.now().plusDays(31), 1);
     }
--- src/test/java/com/carlos/HotelReservation/factories/dto/RequestGuestDtoFactory.java
+++ src/test/java/com/carlos/HotelReservation/factories/dto/RequestGuestDtoFactory.java
@@ -4,6 +4,6 @@ import com.carlos.HotelReservation.api.dto.RequestGuestDto;
 
 public class RequestGuestDtoFactory {
     public static RequestGuestDto requestCreateGuest(){
-        return new RequestGuestDto("carlos@email.com","123456");
+        return new RequestGuestDto("carlos@alten.com","123456");
     }
 }
